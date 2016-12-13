23e785a23d85d559d51a7aa452b1b4e81ee5cc9a
jaki2012 <jaki2012@sina.cn>
2016-12-13 14:47:06 +0800

    add html5 template resources and make it set up properly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import '../../public/css/styles.css';
+// 去掉影响body和字体颜色的css
+// import '../../public/css/styles.css';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
